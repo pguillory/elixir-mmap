96ead66645425be95f0ced33e91b69cfc4e96504
Preston Guillory <pguillory@gmail.com>
2024-03-01 20:12:50 -0800

    Add ex_doc dependency

--- mix.exs
+++ mix.exs
@@ -7,7 +7,19 @@ defmodule Mmap.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
+    ]
+  end
+
+  defp package do
+    [
+      description: "Replacement for File.read using mmap",
+      licenses: ["MIT"],
+      maintainers: ["pguillory@gmail.com"],
+      links: %{
+        "GitHub" => "https://github.com/pguillory/elixir-mmap"
+      }
     ]
   end
 
@@ -16,6 +28,8 @@ defmodule Mmap.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 end
